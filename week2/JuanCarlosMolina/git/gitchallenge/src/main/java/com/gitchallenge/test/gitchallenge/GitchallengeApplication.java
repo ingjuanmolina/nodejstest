462817c047dd7558c5f999033017319048b8e4ad
ingjuanmolina <50556513+ingjuanmolina@users.noreply.github.com>
2019-06-21 23:16:26 -0500

    Add files via upload

---
+++ week2/JuanCarlosMolina/git/gitchallenge/src/main/java/com/gitchallenge/test/gitchallenge/GitchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gitchallenge.test.gitchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitchallengeApplication.class, args);
+	}
+
+}
